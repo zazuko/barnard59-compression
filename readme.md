dc0d127d3cd87574b6dd8322607b92655dd29f5f
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-03-04 15:32:59 +0100

    feat: moved original code

---
+++ readme.md
@@ -0,0 +1,42 @@
+# banard59-compression
+
+Handling compression streams in barnard59 pipelines
+
+## Unzip a compressed stream
+
+Transforms a compressed zip stream into a new stream where each chunk is a stream of a file's contents
+
+### Step
+
+```
+<> a p:Step;
+  code:implementedBy [ a code:EcmaScript;
+  code:link <node:barnard59-compression/zip.js#unzip>
+].
+```
+
+## Unzips first file from a compressed stream
+
+Transforms a compressed zip stream into a stream of the first file found in the compresed input matching a pattern
+
+### Step
+
+```
+<> a p:Step;
+  code:implementedBy [ a code:EcmaScript;
+  code:link <node:barnard59-compression/zip.js#unzipOne>
+].
+```
+
+## Process uncompressed files with sub pipeline
+
+Use in combination with #unzip to process every file using the given sub pipeline. Optionally, provide a second argument to decide whether any given file should be skipped
+
+### Step
+
+```
+<> a p:Step;
+  code:implementedBy [ a code:EcmaScript;
+  code:link <node:barnard59-compression/zip.js#transform>
+].
+```
---
+++ zip.js
@@ -0,0 +1,50 @@
+import unzipper from 'unzipper'
+import stream from 'readable-stream'
+import Core from 'barnard59-core'
+
+export function unzip () {
+    return unzipper.Parse()
+}
+
+export function unzipOne (pattern) {
+    return unzipper.ParseOne(pattern)
+}
+
+export function extract (destinationDir) {
+    return unzipper.Extract({ path: destinationDir })
+}
+
+class UnzipTransform extends stream.Transform {
+    constructor (subPipeline, shouldProcess) {
+        super({ objectMode: true })
+
+        this.shouldProcess = shouldProcess
+        this.subPipeline = subPipeline
+    }
+
+    _transform (entry, e, cb) {
+        if (this.shouldProcess && this.shouldProcess(entry.path) === false) {
+            console.log(`Skipping file ${entry.path}`)
+            entry.autodrain()
+            cb()
+            return
+        }
+
+        const nextStream = Core.pipeline(this.subPipeline.node._context[0].dataset, {
+            iri: this.subPipeline.node.term,
+            basePath: this.subPipeline.basePath,
+            context: this.subPipeline.context,
+            variables: this.subPipeline.variables
+        })
+        nextStream.variables.set('csv', entry.path)
+
+        nextStream._init().then(() => {
+            console.log(`Start processing file ${entry.path}`)
+            entry.pipe(nextStream.streams[0]).on('finish', cb)
+        })
+    }
+}
+
+export function transform (subPipeline, shouldProcess) {
+    return new UnzipTransform(subPipeline, shouldProcess)
+}
